b6c9bfbc6246b5b4fac9110ff5825340aef06382
USuck <yoUSuck.0327@gmail.com>
2019-03-26 01:34:12 +0900

    Add Runs and Mlflow

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ class DataBricks {
 	}
 	
 	requestUrl(path) {
-		return `https://${this.domain}/api/${this.version}/${path}`
+		return `https://${this.domain}/api/${this.version}/${this.path||''}${path}`
 	}
 	
 	async req(method, path, param){
@@ -43,16 +43,41 @@ class DataBricks {
 		return new Runs({domain: this.domain, version: this.version, token: this.token})
 	}
 	
+	get Mlflow() {
+		return new Mlflow({domain: this.domain, version: this.version, token: this.token})
+	}
+	
 }
 
 class Jobs extends DataBricks {
 	
 	constructor(args) {
 		super(args)
+		this.path = 'jobs'
 	}
 	
-	runNow({job_id, ...params}) {
-		return this.req('post', 'jobs/run-now', {job_id, ...params})
+	create(param) {
+		return this.req('post', '/create', param)
+	}
+	
+	list() {
+		return this.req('get', '/list')
+	}
+	
+	delete({job_id}) {
+		return this.req('post', '/delete', {job_id})
+	}
+	
+	get({job_id}) {
+		return this.req('get', '/get', {job_id})
+	}
+	
+	reset({job_id, new_settings}) {
+		return this.req('post', '/reset', {job_id, new_settings})
+	}
+	
+	runNow({job_id, ...param}) {
+		return this.req('post', '/run-now', {job_id, ...param})
 	}
 	
 }
@@ -61,18 +86,78 @@ class Runs extends DataBricks {
 	
 	constructor(args) {
 		super(args)
+		this.path = 'jobs/runs'
+	}
+	
+	submit(param) {
+		return this.req('post', `/submit`, param)
+	}
+	
+	list(param) {
+		return this.req('get', `/list`, param)
 	}
 	
 	get({run_id}) {
-		return this.req('get', 'jobs/runs/get', {run_id})
+		return this.req('get', `/get`, {run_id})
 	}
 	
-	getOutput({run_id}) {
-		return this.req('get', 'jobs/runs/get-output', {run_id})
+	export({run_id, views_to_export='CODE'}) {
+		return this.req('get', `/export`, {run_id, views_to_export})
 	}
 	
 	cancel({run_id}) {
-		return this.req('get', 'jobs/runs/cancel', {run_id})
+		return this.req('post', `/cancel`, {run_id})
+	}
+	
+	getOutput({run_id}) {
+		return this.req('get', `/get-output`, {run_id})
+	}
+	
+	delete({run_id}) {
+		return this.req('post', `/delete`, {run_id})
+	}
+	
+}
+
+class Mlflow extends DataBricks {
+	
+	constructor(args) {
+		super(args)
+		this.path = 'preview/mlflow'
+	}
+	
+	get Experiment() {
+		return new Experiment({domain: this.domain, version: this.version, token: this.token})
+	}
+	
+	get Runs() {
+		return new MlflowRuns({domain: this.domain, version: this.version, token: this.token})
+	}
+	
+}
+
+class Experiment extends Mlflow {
+	
+	constructor(args) {
+		super(args)
+		this.path = 'preview/mlflow/experiments'
+	}
+	
+	get({experiment_id}) {
+		return this.req('get', '/get', {experiment_id})
+	}
+	
+}
+
+class MlflowRuns extends Mlflow {
+	
+	constructor(args) {
+		super(args)
+		this.path = 'preview/mlflow/runs'
+	}
+	
+	search({experiment_ids, ...param}) {
+		return this.req('post', '/search', {experiment_ids, ...param})
 	}
 	
 }
